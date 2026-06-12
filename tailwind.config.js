17d426d6e2a1a5fd17a0d59b441f404b0031f628
sanirudh0902-stack <sanirudh0902@gmail.com>
2026-06-12 19:47:59 +0530

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './insta_ppt/index.html',
+    './insta_ppt/certificate.js',
+    './insta_ppt/src/**/*.{css,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
